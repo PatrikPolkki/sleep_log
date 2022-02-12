41a61c82e2e68379c67a4776c3a797c5c8725849
patep <patrik.polkki@metropolia.fi>
2022-02-12 15:23:14 +0200

    testi

---
+++ app/src/main/java/com/example/sleeplog/ViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.sleeplog
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ViewModel @Inject constructor() : ViewModel() {
+
+}
